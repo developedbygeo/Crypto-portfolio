0f9485b098eb9fdb632b9278263f624627c3680c
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-24 21:24:13 +0200

    style: adding the theme

---
+++ src/shared/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+const myTheme: DefaultTheme = {
+  mainBg: '#1D2024',
+  accent: '#DEC16D',
+  secondary: '#2A2B31',
+  mainText: '#eee',
+  secondaryText: '#606166',
+  black: '#222',
+  deepBlack: '#000000',
+};
+
+export { myTheme };
