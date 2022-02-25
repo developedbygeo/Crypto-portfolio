7d821ddb7f7459cb05670bd5c65939d74ed7001d
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-25 21:53:34 +0200

    updating theme

--- src/shared/theme.ts
+++ src/shared/theme.ts
@@ -1,13 +1,24 @@
 import { DefaultTheme } from 'styled-components';
 
 const myTheme: DefaultTheme = {
-  mainBg: '#1D2024',
-  accent: '#DEC16D',
-  secondary: '#2A2B31',
-  mainText: '#eee',
-  secondaryText: '#606166',
-  black: '#222',
-  deepBlack: '#000000',
+  typography: {
+    weights: {
+      thin: 100,
+      light: 300,
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+  colors: {
+    mainBg: '#1D2024',
+    accent: '#DEC16D',
+    secondary: '#2A2B31',
+    mainText: '#eee',
+    secondaryText: '#606166',
+    black: '#222',
+    deepBlack: '#000000',
+  },
 };
 
 export { myTheme };
