9b78823d72c475f6d0aab4d31ee42ca8e1e7b6d7
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-25 21:53:13 +0200

    chore: extending the default theme

--- src/theme.d.ts
+++ src/theme.d.ts
@@ -2,12 +2,23 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    mainBg: string;
-    accent: string;
-    secondary: string;
-    mainText: string;
-    secondaryText: string;
-    black: string;
-    deepBlack: string;
+    typography: {
+      weights: {
+        thin: number;
+        light: number;
+        regular: number;
+        medium: number;
+        bold: number;
+      };
+    };
+    colors: {
+      mainBg: string;
+      accent: string;
+      secondary: string;
+      mainText: string;
+      secondaryText: string;
+      black: string;
+      deepBlack: string;
+    };
   }
 }
