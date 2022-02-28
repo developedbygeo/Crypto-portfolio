9d465681a2fbd9f37d7bff12b1f6aa5d08423bfa
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-01 01:17:19 +0200

    chore: fixing imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './shared/globalStyle';
@@ -7,17 +8,17 @@ import { myTheme } from './shared/theme';
 import Header from './components/Header/Header';
 
 function App() {
-  return (
-    <ThemeProvider theme={myTheme}>
-      <GlobalStyle />
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-        </Routes>
-      </main>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={myTheme}>
+            <GlobalStyle />
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                </Routes>
+            </main>
+        </ThemeProvider>
+    );
 }
 
 export default App;
