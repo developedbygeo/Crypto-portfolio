e7a53b9dbf332fd93b48ddd67626bd2dc991f5d0
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-01 01:15:00 +0200

    style: adding the interface and effects for the buttons

--- src/components/UI/Button.styled.ts
+++ src/components/UI/Button.styled.ts
@@ -1,49 +1,62 @@
 import styled, { css } from 'styled-components';
+import { colorPress } from '../../shared/mixins';
 
 interface IBtnProps {
-  readonly ctaAltHover?: Boolean;
-  readonly alignSelf?: string;
+    ctaAltHover?: boolean;
+    alignSelf?: string;
 }
 
-const standardHover = css`
-  background: ${({ theme }) => theme.colors.mainText};
+const standardEffect = css`
+    background: ${({ theme }) => theme.colors.mainText};
 `;
-const altHover = css`
-  background: ${({ theme }) => theme.colors.accent};
-  color: ${({ theme }) => theme.colors.mainBg};
-  border: 1.5px solid ${({ theme }) => theme.colors.accent};
+const altEffect = css`
+    background: ${({ theme }) => theme.colors.accent};
+    color: ${({ theme }) => theme.colors.mainBg};
+    border: 1.5px solid ${({ theme }) => theme.colors.accent};
 `;
 
-const getCtaHoverClr = ({ ctaAltHover }: IBtnProps) => {
-  if (ctaAltHover) {
-    return altHover;
-  }
-  return standardHover;
+const getCtaEffect = ({ ctaAltHover }: IBtnProps) => {
+    if (ctaAltHover) {
+        return altEffect;
+    }
+    return standardEffect;
 };
 
 const commonBtnStyling = css<IBtnProps>`
-  letter-spacing: 0.125rem;
-  text-align: center;
-  max-width: 100%;
-  padding: 1.5rem 2.5rem;
-  max-width: 30rem;
-  min-width: 2rem;
-  min-height: 2rem;
-  color: ${({ theme }) => theme.colors.mainText};
-  align-self: ${({ alignSelf }) => alignSelf || 'inherit'};
+    letter-spacing: 0.125rem;
+    text-align: center;
+    max-width: 100%;
+    padding: 1.5rem 2.5rem;
+    max-width: 30rem;
+    min-width: 2rem;
+    min-height: 2rem;
+    color: ${({ theme }) => theme.colors.mainText};
+    align-self: ${({ alignSelf }) => alignSelf || 'inherit'};
 `;
 
-export const CtaButton = styled.button<IBtnProps>`
-  border: 1.5px solid ${({ theme }) => theme.colors.mainText};
-  color: ${({ theme }) => theme.colors.mainText};
-  font-weight: 700;
-  border-radius: 0.15rem;
-  background: transparent;
-  transition: all 400ms ease-in-out;
-  font-size: 2.4rem;
-  align-self: ${({ alignSelf }) => alignSelf || 'initial'};
-  ${commonBtnStyling};
-  &:hover {
-    ${getCtaHoverClr};
-  }
+export const CtaButton = styled.button`
+    border: 1.5px solid ${({ theme }) => theme.colors.mainText};
+    color: ${({ theme }) => theme.colors.mainText};
+    font-weight: 700;
+    border-radius: 0.15rem;
+    background: transparent;
+    transition: all 200ms ease-in-out;
+    font-size: 2.4rem;
+    align-self: ${({ alignSelf }) => alignSelf || 'initial'};
+    ${commonBtnStyling};
+    ${colorPress}
+    @media (hover: hover) {
+        &:hover {
+            ${getCtaEffect};
+        }
+    }
+    &:active {
+        ${getCtaEffect}
+    }
+`;
+export const EmptyButton = styled.button`
+    width: auto;
+    height: auto;
+    ${colorPress}
+    ${getCtaEffect}
 `;
