043078c701b525569a40899cf4f0dbb3ce7770dc
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-01 01:20:50 +0200

    chore: fixing imports and adding classnames to wrappers

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,21 +1,22 @@
+import React from 'react';
 import StyledLanding from './Landing.styled';
 import backgroundImage from '../assets/backgroundLanding.webp';
 import { CtaButton } from '../components/UI/Button.styled';
 
 const Landing = () => {
-  return (
-    <StyledLanding bg={backgroundImage}>
-      <div>
-        <h1>
-          Stay <span>up to date</span> with your crypto.
-        </h1>
-        <h2>Track your portfolio performance here.</h2>
-      </div>
-      <div>
-        <CtaButton ctaAltHover>Get Started</CtaButton>
-      </div>
-    </StyledLanding>
-  );
+    return (
+        <StyledLanding bg={backgroundImage}>
+            <div className="text-wrap">
+                <h1>
+                    Stay <span>up to date</span> with your crypto.
+                </h1>
+                <h2>Track your portfolio performance here.</h2>
+            </div>
+            <div className="cta-wrap">
+                <CtaButton ctaAltHover>Get Started</CtaButton>
+            </div>
+        </StyledLanding>
+    );
 };
 
 export default Landing;
