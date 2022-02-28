a8aac35856139dd1ae78971f07c00f9dd513277e
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-01 01:16:18 +0200

    feat: adding the overall header layout with the navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,34 @@
+import React, { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 
+import Nav from './Nav';
 import { ReactComponent as LogoSVG } from '../../assets/safe.svg';
 import { Menu } from '@styled-icons/material-rounded/Menu';
 import StyledHeader from './Header.styled';
+import { EmptyButton } from '../UI/Button.styled';
 
 const Header = () => {
-  return (
-    <StyledHeader>
-      <div>
-        <Link to="/" title="Home">
-          <LogoSVG />
-        </Link>
-        <button title="Toggle menu">
-          <Menu />
-        </button>
-      </div>
-    </StyledHeader>
-  );
+    const [isNavOpen, setNavIsOpen] = useState<boolean>(false);
+
+    const navToggleHandler = () => setNavIsOpen((prevState) => !prevState);
+
+    return (
+        <>
+            <StyledHeader>
+                <div className="container">
+                    <Link to="/" className="logo">
+                        <LogoSVG />
+                    </Link>
+                    <EmptyButton className="menu" title="Toggle menu" onClick={navToggleHandler}>
+                        <Menu />
+                    </EmptyButton>
+                    <Nav className="desktop-nav" />
+                    {isNavOpen && <Nav className="mobile-nav" />}
+                </div>
+            </StyledHeader>
+        </>
+    );
 };
 
 export default Header;
