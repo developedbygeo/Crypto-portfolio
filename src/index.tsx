f2d07850d80d679fb5cbdac4d34a357138ce00de
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-02 04:16:01 +0200

    chore: moving global imports to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import { ThemeProvider } from 'styled-components';
+import { myTheme } from './shared/theme';
+import GlobalStyle from './shared/globalStyle';
+import App from './App';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <ThemeProvider theme={myTheme}>
+                    <GlobalStyle />
+                    <App />
+                </ThemeProvider>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
