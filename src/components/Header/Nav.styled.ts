0639620caa3dae634240c3de24824d2777c59271
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-02 04:22:26 +0200

    style: minor changes to the nav and adding media query for tablets

--- src/components/Header/Nav.styled.ts
+++ src/components/Header/Nav.styled.ts
@@ -1,11 +1,12 @@
 import styled, { css } from 'styled-components';
 
 import { flexMixin, colorInteract, showActive } from '../../shared/mixins';
+import { devices } from '../../shared/breakpoints';
 import { IAppProps } from '../../shared/models/props.interface';
 
 const desktopNav = css`
     ${flexMixin('space-evenly', 'center', 'row')};
-    min-width: 45vw;
+    min-width: 70vw;
     & > .divider {
         width: 0.1rem;
         height: 2rem;
@@ -20,8 +21,8 @@ const mobileNav = css`
     height: 90vh;
     width: 30vw;
     padding: 0 2rem;
-    background: rgba(0, 0, 0, 0.87);
-    backdrop-filter: saturate(180%) blur(15px);
+    background: rgba(0, 0, 0, 0.9);
+    backdrop-filter: blur(15px);
     ${flexMixin('flex-start', 'flex-start', 'column')};
     gap: 5rem;
     border-left: 0.5px solid rgba(${({ theme }) => theme.colors.accentRGB}, 0.25);
@@ -31,6 +32,9 @@ const mobileNav = css`
         margin-top: 2rem;
     }
     & > .divider {
+        display: none;
+    }
+    @media ${devices.tablet} {
         display: none;
     }
 `;
