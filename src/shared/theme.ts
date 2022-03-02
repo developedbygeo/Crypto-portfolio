dfb676b03e10ed8cbd74d02d8ab232596d5fea8e
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-02 04:17:39 +0200

    style: cchanging the mainBg color

--- src/shared/theme.ts
+++ src/shared/theme.ts
@@ -1,25 +1,25 @@
 import { DefaultTheme } from 'styled-components';
 
 const myTheme: DefaultTheme = {
-  typography: {
-    weights: {
-      thin: 100,
-      light: 300,
-      regular: 400,
-      medium: 500,
-      bold: 700,
+    typography: {
+        weights: {
+            thin: 100,
+            light: 300,
+            regular: 400,
+            medium: 500,
+            bold: 700,
+        },
     },
-  },
-  colors: {
-    mainBg: '#1D2024',
-    accent: '#DEC16D',
-    accentRGB: '222, 193, 109',
-    secondary: '#2A2B31',
-    mainText: '#eee',
-    secondaryText: '#606166',
-    black: '#222',
-    deepBlack: '#000000',
-  },
+    colors: {
+        mainBg: '#151515',
+        accent: '#DEC16D',
+        accentRGB: '222, 193, 109',
+        secondary: '#2A2B31',
+        mainText: '#eee',
+        secondaryText: '#606166',
+        black: '#222',
+        deepBlack: '#000000',
+    },
 };
 
 export { myTheme };
