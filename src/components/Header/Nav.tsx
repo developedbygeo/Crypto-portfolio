07ae2f9766cd3fcf31ba0364d0005c95405145f0
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-02 04:20:15 +0200

    feat: adding more navlinks to the nav

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { IAppProps } from '../../shared/models/props.interface';
@@ -7,11 +6,15 @@ import StyledNav from './Nav.styled';
 const Nav = ({ className }: IAppProps) => {
     return (
         <StyledNav className={className}>
-            <NavLink to="/portfolio">Portfolio</NavLink>
-            <div className="divider"></div>
             <NavLink to="/markets">Markets</NavLink>
             <div className="divider"></div>
-            <NavLink to="/transactions">Transactions </NavLink>
+            <NavLink to="/portfolio">Portfolio</NavLink>
+            <div className="divider"></div>
+            <NavLink to="/transactions">Transactions</NavLink>
+            <div className="divider"></div>
+            <NavLink to="/calculator">Calculator</NavLink>
+            <div className="divider"></div>
+            <NavLink to="/getting-started">Get Started</NavLink>
         </StyledNav>
     );
 };
