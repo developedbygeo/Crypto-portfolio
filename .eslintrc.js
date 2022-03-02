624033990590fcb7e3708f18f36f293c6145372e
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-02 04:08:50 +0200

    chore: updating settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
+    parser: '@typescript-eslint/parser',
+    extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
-  },
-  rules: {},
-  settings: {
-    react: {
-      version: 'detect',
+    rules: {
+        'react/jsx-uses-react': 0,
+        'react/react-in-jsx-scope': 0,
     },
-  },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 };
