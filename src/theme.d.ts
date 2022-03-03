fb9cad27d41896c77b972f9416777d75d0f91648
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-03 21:24:01 +0200

    chore: updating theme type to accept two more clr variables

--- src/theme.d.ts
+++ src/theme.d.ts
@@ -1,25 +1,27 @@
 import 'styled-components';
 
 declare module 'styled-components' {
-  export interface DefaultTheme {
-    typography: {
-      weights: {
-        thin: number;
-        light: number;
-        regular: number;
-        medium: number;
-        bold: number;
-      };
-    };
-    colors: {
-      mainBg: string;
-      accent: string;
-      accentRGB: string;
-      secondary: string;
-      mainText: string;
-      secondaryText: string;
-      black: string;
-      deepBlack: string;
-    };
-  }
+    export interface DefaultTheme {
+        typography: {
+            weights: {
+                thin: number;
+                light: number;
+                regular: number;
+                medium: number;
+                bold: number;
+            };
+        };
+        colors: {
+            mainBg: string;
+            accent: string;
+            accentRGB: string;
+            secondary: string;
+            mainText: string;
+            secondaryText: string;
+            black: string;
+            deepBlack: string;
+            stonks: string;
+            notStonks: string;
+        };
+    }
 }
