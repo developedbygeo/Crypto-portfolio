e7532de889404469bc8c539d6763f23fc5691638
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-03 22:00:04 +0200

    feat: finished the markets header with the search menu

--- src/pages/Markets.tsx
+++ src/pages/Markets.tsx
@@ -1,19 +1,32 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 import { StyledSection } from '../components/UI/Section.styled';
 import { EmptyButton } from '../components/UI/Button.styled';
+import { StyledSearch } from '../components/UI/Search.styled';
 import { SearchAlt } from '@styled-icons/boxicons-regular/SearchAlt';
 
 const Markets = () => {
-    // const [isSearchActive, setIsSearchActive] = useState(false);
+    const [isSearchActive, setIsSearchActive] = useState(false);
+
+    const searchActiveHandler = () => setIsSearchActive((prevState) => !prevState);
 
     return (
         <StyledSection>
             <div className="markets-header">
                 <h2>Markets</h2>
-                <EmptyButton>
-                    <SearchAlt size="3rem" />
-                </EmptyButton>
+                <div className="search-wrap">
+                    {isSearchActive && (
+                        <StyledSearch>
+                            <input type="text" placeholder="By ID or name" />
+                            <button className={isSearchActive && 'activeBtn'} type="submit">
+                                Search
+                            </button>
+                        </StyledSearch>
+                    )}
+                    <EmptyButton onClick={searchActiveHandler}>
+                        <SearchAlt size={17} />
+                    </EmptyButton>
+                </div>
             </div>
         </StyledSection>
     );
